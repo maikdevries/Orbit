29a161ed2979d1b677f7556fd3fe3ffbbe9d9fed
maikdevries <dev-maik.devries@outlook.com>
2021-04-06 00:30:51 +0200

    Resolve #3 - Create Discord guild selection flow

--- src/js/getDiscord.js
+++ src/js/getDiscord.js
@@ -1,6 +1,21 @@
+async function getUser () {
+	try {
+		const res = await fetch('https://discord.com/api/users/@me', { headers: { Authorization: `${localStorage.getItem('tokenType')} ${localStorage.getItem('accessToken')}` } });
+		return await res.json();
+	} catch (error) { console.error(error) }
+}
+
+async function getFilteredGuilds () {
+	return filterGuilds(await getGuilds());
+}
+
 async function getGuilds () {
-	await fetch('https://discord.com/api/users/@me', { headers: { Authorization: `${localStorage.getItem('tokenType')} ${localStorage.getItem('accessToken')}` } }).then(async (res) => {
-		const { username } = await res.json();
-		document.getElementById('welcome').innerText = `Welcome to Orbit, ${username}!`
-	}).catch((error) => console.error(error));
+	try {
+		const res = await fetch(`https://discord.com/api/users/@me/guilds`, { headers: { Authorization: `${localStorage.getItem('tokenType')} ${localStorage.getItem('accessToken')}` } });
+		return await res.json();
+	} catch (error) { console.error(error) }
+}
+
+function filterGuilds (guilds) {
+	return guilds.filter((guild) => guild.owner || (guild.permissions & 0x20) === 0x20 || (guild.permissions & 0x8) === 0x8);
 }
