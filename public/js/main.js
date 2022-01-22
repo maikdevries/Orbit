b1f2ddddbeb20c17aa7e283723545156bc690b2a
maikdevries <dev.maik.devries@outlook.com>
2022-01-22 19:33:28 +0100

    Close #31 - Drop-down menu on user info

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+const openDropDownEventListener = ((event) => {
+	event.stopPropagation();
+
+	const dropDownElementStyle = document.getElementById('userDropdown').style;
+	dropDownElementStyle.visibility = 'visible';
+	dropDownElementStyle.opacity = 1;
+
+	document.getElementById('userProfile').onclick = null;
+	document.onclick = closeDropDownEventListener;
+});
+
+const closeDropDownEventListener = ((event) => {
+	event.stopPropagation();
+
+	if (event.target.matches('#userDropdown')) return;
+
+	const dropDownElementStyle = document.getElementById('userDropdown').style;
+	dropDownElementStyle.visibility = 'hidden';
+	dropDownElementStyle.opacity = 0;
+
+	document.onclick = null;
+	document.getElementById('userProfile').onclick = openDropDownEventListener;
+});
+
+document.getElementById('userProfile').onclick = openDropDownEventListener;
